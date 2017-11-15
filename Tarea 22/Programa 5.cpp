84a319d6570863934a595c62142175912451fb2b
raygadasbaezrodrigo <31478555+raygadasbaezrodrigo@users.noreply.github.com>
2017-11-14 22:58:06 -0600

    Add files via upload

---
+++ Tarea 22/Programa 5.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"info.h"
+#define MAX 10
+int main ()
+{
+	appInfoData("Ejemplo de punteros y arreglos", "14/11/2017");
+	
+	int cals [MAX] = {8,9,8,8,6,7,9,8,10,10};
+	int *ptrCals;
+	int j=0;
+	ptrCals=cals; //&cals[0]
+	printf("Elemento\t Direccion\t valor\n");
+	for (j=0; j<MAX;j++)
+		printf("%d              \t%d \t %d\n",j,&cals[j],cals[j]);
+		
+		printf("---------------------\n");
+		
+		printf("Direccion a la que apunta: %d\n",ptrCals);
+		printf("Valor a la que apunta: %d\n",*ptrCals);
+		ptrCals=ptrCals+4;
+		printf("Direccion a la que apunta: %d\n",ptrCals);
+		printf("Valor a la que apunta: %d\n",*ptrCals);
+		
+		return 0;	
+}
---
+++ Tarea 22/info.h
@@ -0,0 +1,10 @@
+void appInfoData(char *titulo,char *fecha)
+{
+  printf("----------------------------------------------------------------\n");
+  printf("_\n");
+  printf("_       %s\n", titulo);
+  printf("_       Rodrigo Raygadas Baez ®\n");
+  printf("_       %s\n", fecha);
+  printf("_\n");
+  printf("----------------------------------------------------------------\n");
+}
